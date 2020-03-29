402d7568c257498b199def2ec8f98ebc5621fa59
b2 <brendan@mobilewebsmart.com>
2020-03-29 08:07:58 -0600

    Web app UI layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/* <img src={logo} className="App-logo" alt="logo" /> */}
+				<h1>Save Links for Later</h1>
+				<p>What do you want to read, watch or listen to?</p>
+			</header>
+			<div className="App-body">
+				<a
+					href="https://example.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Example.com
+        		</a>
+			</div>
+			<footer className="App-footer">
+				Copyright &copy; 2020
+			</footer>
+		</div>
+	);
 }
 
 export default App;
