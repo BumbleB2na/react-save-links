8a9c34fc9f01ba80dafb75e01dd499347e1f87ae
b2 <brendan@mobilewebsmart.com>
2020-03-29 15:55:08 -0600

    Updates

---
+++ precache-manifest.65267cc2abd69116f0f8c864de902054.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d027085ccd6890258be7fcf99eb21a18",
+    "url": "/react-save-links/index.html"
+  },
+  {
+    "revision": "97a1418b48b39d0aadab",
+    "url": "/react-save-links/static/css/main.61611871.chunk.css"
+  },
+  {
+    "revision": "67a12968b4550fe6c87d",
+    "url": "/react-save-links/static/js/2.1964e763.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-save-links/static/js/2.1964e763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a1418b48b39d0aadab",
+    "url": "/react-save-links/static/js/main.7d2e8645.chunk.js"
+  },
+  {
+    "revision": "37bc0bbcb6496fef7836",
+    "url": "/react-save-links/static/js/runtime-main.93e419e8.js"
+  }
+]);
